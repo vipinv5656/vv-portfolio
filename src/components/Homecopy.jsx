336437ab27bab7c0b787c90474c1840fe53ab9ef
vipin varghese <vipin.varghese@student.griffith.ie>
2023-04-02 22:15:11 +0100

    updated cv

--- src/components/Homecopy.jsx
+++ src/components/Homecopy.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import HeroImage from "../assets/imageprofile.jpg";
 import { MdOutlineKeyboardArrowRight,MdBookmarks } from "react-icons/md";
 import { Link } from "react-scroll";
-import resume from "../assets/vipinReactCV.pdf";
+import resume from "../assets/VipinVarghese.pdf";
 
 
 const Home = () => {
