834507af9dfbd9b74c0abdb150cd622d7d97889d
vipin varghese <vipin.varghese@student.griffith.ie>
2023-07-28 18:07:05 +0100

    some id change

--- src/components/SocialLinks copy.jsx
+++ src/components/SocialLinks copy.jsx
@@ -34,8 +34,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      // href: "/resume.pdf",
-      href: resume,
+         href: resume,
       style: " rounded-br",
       download: true,
     },
